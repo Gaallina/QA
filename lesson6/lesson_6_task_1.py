0a1c815a15ab4fb402cee3e083c4086fb2f17c32
Galina <154439882+Gaallina@users.noreply.github.com>
2024-09-08 20:51:46 +0900

    Update lesson_6_task_1.py

--- lesson6/lesson_6_task_1.py
+++ lesson6/lesson_6_task_1.py
@@ -7,18 +7,14 @@ options = webdriver.ChromeOptions()
 options.add_argument('--ignore-ssl-errors=yes')
 options.add_argument('--ignore-certificate-errors')
 
-driver = webdriver.Chrome(options=options)
 driver = webdriver.Chrome(
-    service=ChromeService(ChromeDriverManager().install()))
+    service=ChromeService(ChromeDriverManager().install()),
+    options=options)
 
 driver.implicitly_wait(20)
-
 driver.get("http://www.uitestingplayground.com/ajax")
+
 driver.find_element(By.CSS_SELECTOR, "#ajaxButton").click()
-
-content = driver.find_element(By.CSS_SELECTOR, "#content")
-txt = content.find_element(By.CSS_SELECTOR, "p.bg-success").text
-
-print(txt)
+print(driver.find_element(By.CSS_SELECTOR, "p.bg-success").text)
 
 driver.quit()
